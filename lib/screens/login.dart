07e1fd8e2ead0c89323f3ff18e1cccc2e19059ef
Alvin Zhafif Afilla <villalvin02@gmail.com>
2023-12-01 12:27:20 +0700

    fixed login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -113,7 +113,7 @@ class _LoginPageState extends State<LoginPage> {
                     // To connect the Android emulator to Django on localhost,
                     // use the URL http://10.0.2.2/
                     final response = await request
-                        .login("http://localhost:8000/auth/login/", {
+                        .login("http://127.0.0.1:8000/auth/login/", {
                       'username': username,
                       'password': password,
                     });

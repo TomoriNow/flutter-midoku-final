d0323f7c9bbc95cc1a94ad8fde0c61d2b74d12fc
Sirered <gikurniawan.1995@gmail.com>
2023-12-04 16:44:41 +0700

    implementing delete

--- lib/screens/collection.dart
+++ lib/screens/collection.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:midoku/models/book_entry.dart';
 import 'package:midoku/screens/add_custom_entry.dart';
+import 'package:midoku/screens/detailed_entry.dart';
 import 'package:midoku/widgets/book_entry_card.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
@@ -14,22 +16,22 @@ class CollectionPage extends StatefulWidget {
 }
 
 class _CollectionPageState extends State<CollectionPage> {
+  late Future<List<BookEntry>> _bookEntries;
   Future<List<BookEntry>> fetchItem() async {
-    // TODO: Change the URL to your Django app's URL. Don't forget to add the trailing slash (/) if needed.
-    final request = context.watch<CookieRequest>();
-    var response = await request.get(
-      'http://127.0.0.1:8000/Galih/'
-    );
-    // decode the response to JSON
-
-    // convert the JSON to Item object
+    final request = Provider.of<CookieRequest>(context, listen: false);
+    var response = await request.get('http://127.0.0.1:8000/Galih/');
     List<BookEntry> listItem = [];
     for (var d in response) {
-        if (d != null) {
-          listItem.add(BookEntry.fromJson(d));
-        }
+      if (d != null) {
+        listItem.add(BookEntry.fromJson(d));
+      }
     }
     return listItem;
+  }
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _bookEntries = fetchItem();
   }
   @override
   Widget build(BuildContext context) {
@@ -41,12 +43,13 @@ class _CollectionPageState extends State<CollectionPage> {
       ),
       drawer: const LeftDrawer(),
       body: FutureBuilder(
-        future: fetchItem(),
+        future: _bookEntries,
         builder: (context, AsyncSnapshot<List<BookEntry>> snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {// Access the error information
             var error = snapshot.error;
+            print(error);
             // Handle the error condition
             return Text('Error: $error');
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
@@ -62,7 +65,57 @@ class _CollectionPageState extends State<CollectionPage> {
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
                 final bookEntry = snapshot.data![index];
-                return BookEntryCard(bookEntry: bookEntry);
+                return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      elevation: 10,
+      child: Stack(
+        children: [BookEntryCard(bookEntry: bookEntry),Positioned(
+            top: 8.0,
+            right: 8.0,
+            child: Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.info),
+                  onPressed: () async {
+                    // Handle icon button press
+                    final bool? shouldRefresh = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailEntryPage(bookEntry: bookEntry),
+                      ),
+                    );
+                    if (shouldRefresh != null && shouldRefresh) {
+                      // Refresh the card by calling setState
+                      setState(() {});
+                    }
+                  },
+                ),
+                IconButton(onPressed: () async {
+                  final request = Provider.of<CookieRequest>(context, listen: false);
+                  final response = await request.postJson(
+                    "http://127.0.0.1:8000/delete-entry-flutter/",
+                    jsonEncode(<String, dynamic>{
+                      'id': bookEntry.pk,
+                    })
+                  );
+                  if (response['status'] == 'success') {
+                    setState(() {
+                      snapshot.data!.removeAt(index);
+                    });
+                  } else {
+                    ScaffoldMessenger.of(context)
+                    .showSnackBar(const SnackBar(
+                      content:
+                        Text("Something went wrong, please try again."),
+                    ));
+                  }
+                }, icon: Icon(Icons.delete))
+              ]
+            )
+          ),
+        ],
+      ),
+                );
               },
             );
           }
@@ -75,7 +128,9 @@ class _CollectionPageState extends State<CollectionPage> {
             MaterialPageRoute(builder: (context) => const AddCustomPage()),
           );
           if (shouldRefresh != null && shouldRefresh) {
-                  setState(() {});
+                  setState(() {
+                    _bookEntries = fetchItem();
+                  });
                 }
         },
         label: const Text('Add Custom Entry'),
--- lib/widgets/book_entry_card.dart
+++ lib/widgets/book_entry_card.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:midoku/models/book_entry.dart';
 import 'package:midoku/screens/detailed_entry.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 
 class BookEntryCard extends StatefulWidget {
   final BookEntry bookEntry;
@@ -14,13 +18,10 @@ class BookEntryCard extends StatefulWidget {
 class _BookEntryCardState extends State<BookEntryCard> {
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
     CustomEntry book = widget.bookEntry.customEntry ?? widget.bookEntry.catalogEntry!.book;
 
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-      elevation: 10,
-      child: Stack(
-        children: [
+    return 
           InkWell(
             child: Padding(
               padding: const EdgeInsets.all(20.0),
@@ -76,29 +77,7 @@ class _BookEntryCardState extends State<BookEntryCard> {
                 ),
               ),
             ),
-          ),
-          Positioned(
-            top: 8.0,
-            right: 8.0,
-            child: IconButton(
-              icon: Icon(Icons.info),
-              onPressed: () async {
-                // Handle icon button press
-                final bool? shouldRefresh = await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DetailEntryPage(bookEntry: widget.bookEntry),
-                  ),
-                );
-                if (shouldRefresh != null && shouldRefresh) {
-                  // Refresh the card by calling setState
-                  setState(() {});
-                }
-              },
-            ),
-          ),
-        ],
-      ),
-    );
+          );
+          
   }
 }

9ccc8eaa58e09f6eeef3f0018a7a6dd584dcb1bc
Alvin Zhafif Afilla <villalvin02@gmail.com>
2023-12-05 19:10:20 +0700

    made filter by types

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -6,32 +6,34 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:midoku/screens/add_catalog_entry.dart';
 
-List<Book> bookFromJson(String str) => List<Book>.from(json.decode(str).map((x) => Book.fromJson(x)));
+List<Book> bookFromJson(String str) =>
+    List<Book>.from(json.decode(str).map((x) => Book.fromJson(x)));
 
-String bookToJson(List<Book> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String bookToJson(List<Book> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class Book {
-    int id;
-    List<String> taggits;
-    String name;
-    String imagelink;
-    Type type;
-    String author;
-    String description;
-    String tags;
+  int id;
+  List<String> taggits;
+  String name;
+  String imagelink;
+  Type type;
+  String author;
+  String description;
+  String tags;
 
-    Book({
-        required this.id,
-        required this.taggits,
-        required this.name,
-        required this.imagelink,
-        required this.type,
-        required this.author,
-        required this.description,
-        required this.tags,
-    });
+  Book({
+    required this.id,
+    required this.taggits,
+    required this.name,
+    required this.imagelink,
+    required this.type,
+    required this.author,
+    required this.description,
+    required this.tags,
+  });
 
-    factory Book.fromJson(Map<String, dynamic> json) => Book(
+  factory Book.fromJson(Map<String, dynamic> json) => Book(
         id: json["id"],
         taggits: List<String>.from(json["taggits"].map((x) => x)),
         name: json["name"],
@@ -40,9 +42,9 @@ class Book {
         author: json["author"],
         description: json["description"],
         tags: json["tags"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "taggits": List<dynamic>.from(taggits.map((x) => x)),
         "name": name,
@@ -51,131 +53,125 @@ class Book {
         "author": author,
         "description": description,
         "tags": tags,
-    };
-    
-    Widget buildCatalogWidget(BuildContext context) {
-      return Card(
-        margin: const EdgeInsets.symmetric(
-          horizontal: 16, vertical: 12,
-        ),
-        elevation: 10,
-        child: Stack(
-          children: [
-            Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: SizedBox(
-              height: 600, // Set the desired height for the card
-              child: ListView(
-                children: [
-                  Text(
-                    name,
-                    style: const TextStyle(
-                      fontSize: 23,
-                      fontWeight: FontWeight.bold,
-                    ),
+      };
+
+  Widget buildCatalogWidget(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 12,
+      ),
+      elevation: 10,
+      child: Stack(children: [
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SizedBox(
+            height: 600, // Set the desired height for the card
+            child: ListView(
+              children: [
+                Text(
+                  name,
+                  style: const TextStyle(
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold,
                   ),
-                  const SizedBox(height: 10),
-                  Image.network(
-                    imagelink, // Replace with your image URL
-                    width: 250, // Set the width of the image
-                    height: 400, // Set the height of the image
-                    fit: BoxFit.contain, // BoxFit property to control how the image should be inscribed into the box
+                ),
+                const SizedBox(height: 10),
+                Image.network(
+                  imagelink, // Replace with your image URL
+                  width: 250, // Set the width of the image
+                  height: 400, // Set the height of the image
+                  fit: BoxFit
+                      .contain, // BoxFit property to control how the image should be inscribed into the box
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "Type:",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
                   ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    "Type:",
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  typeValues.reverse[type]!,
+                  style: const TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal,
                   ),
-                  const SizedBox(height: 10),
-                  Text(
-                    typeValues.reverse[type]!,
-                    style: const TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.normal,
-                    ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "Description:",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
                   ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    "Description:",
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  description,
+                  style: const TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal,
                   ),
-                  const SizedBox(height: 10),
-                  Text(
-                    description,
-                    style: const TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.normal,
-                    ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "Tags:",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
                   ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    "Tags:",
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  tags,
+                  style: const TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal,
                   ),
-                  const SizedBox(height: 10),
-                  Text(
-                    tags,
-                    style: const TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-          Positioned(
-            top: 8.0,
-            right: 8.0,
-            child: IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () {
-                // Handle icon button press
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                  builder: (context) =>
-                    AddCatalogPage(book: this),
-                  ),
-                );
-              },
-            ),
-          ),
-          ]
         ),
-      );
-    }
+        Positioned(
+          top: 8.0,
+          right: 8.0,
+          child: IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              // Handle icon button press
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AddCatalogPage(book: this),
+                ),
+              );
+            },
+          ),
+        ),
+      ]),
+    );
+  }
 }
 
-enum Type {
-    MANGA,
-    MANHWA,
-    NOVEL
-}
+enum Type { MANGA, MANHWA, NOVEL }
 
-final typeValues = EnumValues({
-    "Manga": Type.MANGA,
-    "Manhwa": Type.MANHWA,
-    "Novel": Type.NOVEL
-});
+final typeValues = EnumValues(
+    {"Manga": Type.MANGA, "Manhwa": Type.MANHWA, "Novel": Type.NOVEL});
 
 class EnumValues<T> {
-    Map<String, T> map;
-    late Map<T, String> reverseMap;
+  Map<String, T> map;
+  late Map<T, String> reverseMap;
 
-    EnumValues(this.map);
+  EnumValues(this.map);
 
-    Map<T, String> get reverse {
-        reverseMap = map.map((k, v) => MapEntry(v, k));
-        return reverseMap;
-    }
+  Map<T, String> get reverse {
+    reverseMap = map.map((k, v) => MapEntry(v, k));
+    return reverseMap;
+  }
 }
+
+final List<String> types = ["Manwha", "Manga", "Novel"];
---
+++ lib/screens/filtertype.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:midoku/models/book.dart';
+import 'package:midoku/widgets/left_drawer.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+
+class FilterTypePage extends StatefulWidget {
+  const FilterTypePage({Key? key});
+
+  @override
+  State<FilterTypePage> createState() => _FilterTypePageState();
+}
+
+enum Type { MANGA, MANHWA, NOVEL }
+
+class _FilterTypePageState extends State<FilterTypePage> {
+  List<Type> selectedTypes = [];
+  List<Book> filteredBooks = [];
+  List<Book> bookList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // Fetch books once when the widget is created
+    fetchBooks();
+  }
+
+  Future<void> fetchBooks() async {
+    final request = context.read<CookieRequest>();
+    final response =
+        await request.get('http://127.0.0.1:8000/flutter-catalog/');
+
+    setState(() {
+      bookList = []; // Clear existing data
+      for (var d in response) {
+        if (d != null) {
+          bookList.add(Book.fromJson(d));
+        }
+      }
+      // Initialize filteredBooks with all books
+      filteredBooks = List.from(bookList);
+    });
+  }
+
+  void updateFilteredBooks() {
+    setState(() {
+      if (selectedTypes.isEmpty) {
+        // No types selected, show all books
+        filteredBooks = List.from(bookList);
+      } else {
+        // Filter books based on selected types
+        filteredBooks = bookList.where((book) {
+          return selectedTypes.any((selectedType) =>
+              book.type.toString() == selectedType.toString());
+        }).toList();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Filter by Types',
+        ),
+        backgroundColor: Colors.greenAccent,
+        foregroundColor: Colors.white,
+      ),
+      drawer: const LeftDrawer(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Wrap(
+              spacing: 8.0,
+              children: Type.values.map((type) {
+                return FilterChip(
+                  label: Text(type.toString().split('.').last),
+                  selected: selectedTypes.contains(type),
+                  onSelected: (selected) {
+                    if (selected) {
+                      selectedTypes.add(type);
+                    } else {
+                      selectedTypes.remove(type);
+                    }
+                    updateFilteredBooks();
+                  },
+                );
+              }).toList(),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                childAspectRatio: 0.7,
+              ),
+              itemCount: filteredBooks.length,
+              itemBuilder: (context, index) {
+                final catalogBook = filteredBooks[index];
+                return catalogBook.buildCatalogWidget(context);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

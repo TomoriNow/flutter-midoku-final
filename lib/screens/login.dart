1af90cca6b62399cb290825e4ae99c41363c0318
PeakFiction <sakhran.t@gmail.com>
2023-12-11 09:55:18 +0700

    Added Gradient Background to Login and Register

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,3 @@
-// import 'package:shopping_list/screens/menu.dart';
 import 'package:flutter/material.dart';
 import 'package:midoku/screens/catalog.dart';
 import 'package:midoku/main.dart';
@@ -13,7 +12,7 @@ void main() {
 }
 
 class LoginApp extends StatelessWidget {
-  const LoginApp({super.key});
+  const LoginApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +27,7 @@ class LoginApp extends StatelessWidget {
 }
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -42,131 +41,130 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     final request = context.watch<CookieRequest>();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Center(
-        child: Card(
-          elevation: 5.0,
-          shadowColor: Colors.black, // Set the shadow color (optional)
-          shape: RoundedRectangleBorder(
-            borderRadius:
-                BorderRadius.circular(10.0), // Set border radius (optional)
+      body: AnimatedContainer(
+        duration: Duration(seconds: 1),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.blue,
+              Colors.purple,
+            ],
           ),
-          child: Container(
-            padding: const EdgeInsets.all(16.0),
-            width: 300.0,
-            decoration: BoxDecoration(
-              color: Colors.white, // Set the background color
-              borderRadius:
-                  BorderRadius.circular(10.0), // Set border radius (optional)
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey
-                      .withOpacity(0.5), // Set shadow color and opacity
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: Offset(0, 3), // Set the shadow offset
-                ),
-              ],
+        ),
+        child: Center(
+          child: Card(
+            elevation: 5.0,
+            shadowColor: Colors.black,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(
-                  'assets/logos.png', // Replace with your image path
-                  width: 200.0, // Set the width of the image
-                  height: 100.0, // Set the height of the image
-                  fit: BoxFit.cover, // Adjust the BoxFit as needed
-                ),
-                const SizedBox(height: 30.0),
-                TextField(
-                  controller: _usernameController,
-                  decoration: InputDecoration(
-                    labelText: 'Username',
-                    border: OutlineInputBorder(
-                      borderRadius:
-                          BorderRadius.circular(20.0), // Set oval-like shape
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              width: 300.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(
+                    'assets/logos.png',
+                    width: 200.0,
+                    height: 100.0,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(height: 30.0),
+                  TextField(
+                    controller: _usernameController,
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 12.0),
-                TextField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(
-                      borderRadius:
-                          BorderRadius.circular(20.0), // Set oval-like shape
+                  const SizedBox(height: 12.0),
+                  TextField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
                     ),
+                    obscureText: true,
                   ),
-                  obscureText: true,
-                ),
-                const SizedBox(height: 24.0),
-                ElevatedButton(
-                  onPressed: () async {
-                    String username = _usernameController.text;
-                    String password = _passwordController.text;
+                  const SizedBox(height: 24.0),
+                  ElevatedButton(
+                    onPressed: () async {
+                      String username = _usernameController.text;
+                      String password = _passwordController.text;
 
-                    // Check credentials
-                    // TODO: Change the URL and don't forget to add a trailing slash (/) at the end of the URL!
-                    // To connect the Android emulator to Django on localhost,
-                    // use the URL http://10.0.2.2/
-                    final response = await request.login("http://127.0.0.1:8000/auth/login/", {
-                      'username': username,
-                      'password': password,
-                    });
-
-                    if (request.loggedIn) {
-                      String message = response['message'];
-                      String uname = response['username'];
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => CatalogPage()),
+                      final response = await request.login(
+                        "http://127.0.0.1:8000/auth/login/",
+                        {'username': username, 'password': password},
                       );
-                      ScaffoldMessenger.of(context)
-                        ..hideCurrentSnackBar()
-                        ..showSnackBar(
-                          SnackBar(content: Text("$message Welcome, $uname.")),
+
+                      if (request.loggedIn) {
+                        String message = response['message'];
+                        String uname = response['username'];
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => CatalogPage()),
                         );
-                    } else {
-                      showDialog(
-                        context: context,
-                        builder: (context) => AlertDialog(
-                          title: const Text('Login Failed'),
-                          content: Text(response['message']),
-                          actions: [
-                            TextButton(
-                              child: const Text('OK'),
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                            ),
-                          ],
-                        ),
-                      );
-                    }
-                  },
-                  child: const Text('Login'),
-                ),
-                const SizedBox(height: 8.0),
-                Padding(
-                  padding: const EdgeInsets.only(
-                      bottom:
-                          16.0), // Adjust the value for your desired spacing
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const RegisterPage()),
-                      );
+                        ScaffoldMessenger.of(context)
+                          ..hideCurrentSnackBar()
+                          ..showSnackBar(
+                            SnackBar(content: Text("$message Welcome, $uname.")),
+                          );
+                      } else {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: const Text('Login Failed'),
+                            content: Text(response['message']),
+                            actions: [
+                              TextButton(
+                                child: const Text('OK'),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ],
+                          ),
+                        );
+                      }
                     },
-                    child: const Text('Create an account'),
+                    child: const Text('Login'),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 8.0),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 16.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const RegisterPage()),
+                        );
+                      },
+                      child: const Text('Create an account'),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,7 +1,6 @@
-// ignore_for_file: use_build_context_synchronously, library_private_types_in_public_api
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:midoku/screens/login.dart';
-import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
@@ -22,148 +21,141 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     final request = context.watch<CookieRequest>();
-    
-    Future<List<User>> fetchItem() async {
-      final response = await request.get('http://127.0.0.1:8000/other-users/');
-      // convert the JSON to Item object
-      List<User> list_item = [];
-      for (var d in response) {
-        if (d != null) {
-          list_item.add(User.fromJson(d));
-        }
-      }
-      return list_item;
-    }
-    
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Register'),
         backgroundColor: Colors.purple,
         foregroundColor: Colors.white,
       ),
-      body: Center(
-        child: Card(
-          elevation: 5.0,
-          shadowColor: Colors.black, // Set the shadow color (optional)
-          shape: RoundedRectangleBorder(
-            borderRadius:
-                BorderRadius.circular(10.0), // Set border radius (optional)
+      body: AnimatedContainer(
+        duration: Duration(seconds: 1),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.blue,
+              Colors.purple,
+            ],
           ),
-          child: Container(
-            padding: const EdgeInsets.all(16.0),
-            width: 300.0,
-            decoration: BoxDecoration(
-              color: Colors.white, // Set the background color
-              borderRadius:
-                  BorderRadius.circular(10.0), // Set border radius (optional)
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey
-                      .withOpacity(0.5), // Set shadow color and opacity
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: Offset(0, 3), // Set the shadow offset
-                ),
-              ],
+        ),
+        child: Center(
+          child: Card(
+            elevation: 5.0,
+            shadowColor: Colors.black,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(
-                  'assets/logos.png', // Replace with your image path
-                  width: 200.0, // Set the width of the image
-                  height: 100.0, // Set the height of the image
-                  fit: BoxFit.cover, // Adjust the BoxFit as needed
-                ),
-                const SizedBox(height: 30.0),
-                TextField(
-                  controller: _usernameController,
-                  decoration: InputDecoration(
-                    labelText: 'Username',
-                    border: OutlineInputBorder(
-                      borderRadius:
-                          BorderRadius.circular(20.0), // Set oval-like shape
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              width: 300.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(
+                    'assets/logos.png',
+                    width: 200.0,
+                    height: 100.0,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(height: 30.0),
+                  TextField(
+                    controller: _usernameController,
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 12.0),
-                TextField(
-                  controller: _password1Controller,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(
-                      borderRadius:
-                          BorderRadius.circular(20.0), // Set oval-like shape
+                  const SizedBox(height: 12.0),
+                  TextField(
+                    controller: _password1Controller,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
                     ),
+                    obscureText: true,
                   ),
-                  obscureText: true,
-                ),
-                const SizedBox(height: 12.0),
-                TextField(
-                  controller: _password2Controller,
-                  decoration: InputDecoration(
-                    labelText: 'Confirm Password',
-                    border: OutlineInputBorder(
-                      borderRadius:
-                          BorderRadius.circular(20.0), // Set oval-like shape
+                  const SizedBox(height: 12.0),
+                  TextField(
+                    controller: _password2Controller,
+                    decoration: InputDecoration(
+                      labelText: 'Confirm Password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
                     ),
+                    obscureText: true,
                   ),
-                  obscureText: true,
-                ),
-                const SizedBox(height: 24.0),
-                ElevatedButton(
-                  onPressed: () async {
-                    String username = _usernameController.text;
-                    String password1 = _password1Controller.text;
-                    String password2 = _password2Controller.text;
+                  const SizedBox(height: 24.0),
+                  ElevatedButton(
+                    onPressed: () async {
+                      String username = _usernameController.text;
+                      String password1 = _password1Controller.text;
+                      String password2 = _password2Controller.text;
 
-                    final response = await request.postJson("http://127.0.0.1:8000/auth/register/", 
-                    jsonEncode(<String, String>{
-                      'username': username,
-                      'password1': password1,
-                      'password2': password2,
-                      })
-                    );
+                      final response = await request.postJson(
+                        "http://127.0.0.1:8000/auth/register/",
+                        jsonEncode(<String, String>{
+                          'username': username,
+                          'password1': password1,
+                          'password2': password2,
+                        }),
+                      );
 
-                    if (response['status']) {
-                      // Handle successful registration
-                      // You can parse the response body if needed
-                      String message = "Registration successful!";
-                      String uname = username;
-                      ScaffoldMessenger.of(context)
-                        ..hideCurrentSnackBar()
-                        ..showSnackBar(
-                          SnackBar(content: Text("$message Welcome, $uname.")),
+                      if (response['status']) {
+                        String message = "Registration successful!";
+                        String uname = username;
+                        ScaffoldMessenger.of(context)
+                          ..hideCurrentSnackBar()
+                          ..showSnackBar(
+                            SnackBar(content: Text("$message Welcome, $uname.")),
+                          );
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginPage()),
                         );
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const LoginPage()),
-                      );
-                    } else {
-                      // Handle registration failure
-                      String errorMessage = response['message'];
-                      showDialog(
-                        context: context,
-                        builder: (context) => AlertDialog(
-                          title: const Text('Registration Failed'),
-                          content: Text(errorMessage),
-                          actions: [
-                            TextButton(
-                              child: const Text('OK'),
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                            ),
-                          ],
-                        ),
-                      );
-                    }
-                  },
-                  child: const Text('Register'),
-                ),
-              ],
+                      } else {
+                        String errorMessage = response['message'];
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: const Text('Registration Failed'),
+                            content: Text(errorMessage),
+                            actions: [
+                              TextButton(
+                                child: const Text('OK'),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ],
+                          ),
+                        );
+                      }
+                    },
+                    child: const Text('Register'),
+                  ),
+                ],
+              ),
             ),
           ),
         ),

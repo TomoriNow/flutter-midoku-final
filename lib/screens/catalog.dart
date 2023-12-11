1dc07a54a1a37e0e38ee7bab70f8046225e23567
PeakFiction <sakhran.t@gmail.com>
2023-12-11 10:00:23 +0700

    Added Background Gradient in Catalog Page

--- lib/screens/catalog.dart
+++ lib/screens/catalog.dart
@@ -7,27 +7,26 @@ import 'package:provider/provider.dart';
 import 'package:midoku/models/book.dart';
 import 'package:midoku/widgets/left_drawer.dart';
 
-
 class CatalogPage extends StatefulWidget {
-    const CatalogPage({Key? key}) : super(key: key);
+  const CatalogPage({Key? key}) : super(key: key);
 
-    @override
-    _CatalogPageState createState() => _CatalogPageState();
+  @override
+  _CatalogPageState createState() => _CatalogPageState();
 }
 
 class _CatalogPageState extends State<CatalogPage> {
   Future<List<Book>> fetchItem() async {
-      final request = context.watch<CookieRequest>();
-      final response = await request.get('http://127.0.0.1:8000/flutter-catalog/');
+    final request = context.watch<CookieRequest>();
+    final response = await request.get('http://127.0.0.1:8000/flutter-catalog/');
 
-      // convert the JSON to Item object
-      List<Book> list_item = [];
-      for (var d in response) {
-          if (d != null) {
-              list_item.add(Book.fromJson(d));
-          }
+    // convert the JSON to Item object
+    List<Book> list_item = [];
+    for (var d in response) {
+      if (d != null) {
+        list_item.add(Book.fromJson(d));
       }
-      return list_item;
+    }
+    return list_item;
   }
 
   @override
@@ -41,34 +40,45 @@ class _CatalogPageState extends State<CatalogPage> {
         foregroundColor: Colors.white,
       ),
       drawer: const LeftDrawer(),
-      body: FutureBuilder(
-        future: fetchItem(),
-        builder: (context, AsyncSnapshot<List<Book>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {// Access the error information
-      var error = snapshot.error;
-      
-      // Handle the error condition
-      return Text('Error: $error');
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('No item data available.'));
-          } else {
-            return GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                childAspectRatio: 0.7,
-              ),
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                final catalogBook = snapshot.data![index];
-                return catalogBook.buildCatalogWidget(context);
-              },
-            );
-          }
-        },
+      body: AnimatedContainer(
+        duration: Duration(seconds: 1),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.blue,
+              Colors.greenAccent,
+            ],
+          ),
+        ),
+        child: FutureBuilder(
+          future: fetchItem(),
+          builder: (context, AsyncSnapshot<List<Book>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              var error = snapshot.error;
+              return Text('Error: $error');
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return const Center(child: Text('No item data available.'));
+            } else {
+              return GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 0.7,
+                ),
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  final catalogBook = snapshot.data![index];
+                  return catalogBook.buildCatalogWidget(context);
+                },
+              );
+            }
+          },
+        ),
       ),
       floatingActionButton: Stack(
         children: [
